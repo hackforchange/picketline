2324b25927cf4e0711dbb7906a2b7e1a7a373ce1
Andrew Farmer <andrew@moovweb.com>
2011-06-18 12:18:03 -0700

    added fake user info

--- server.rb
+++ server.rb
@@ -6,7 +6,14 @@ module PicketLine
     set :public, File.dirname(__FILE__) + '/static'
     set :views, File.dirname(__FILE__) + '/views'
     
+    before do
+      # TODO: check if the user has a session cookie and set their
+      # user_name and id here.
+      @user = { :name => "andrew", :id => 1 }
+    end
+    
     get '/' do
+      puts @user
       erb :index
     end
   
