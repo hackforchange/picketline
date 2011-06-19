07845877101d89fa049d970cd76b3a94e440b22c
Andrew Farmer <andrew@moovweb.com>
2011-06-19 00:53:50 -0700

    massive improvement to the company page

--- static/javascript/piegraph.js
+++ static/javascript/piegraph.js
@@ -2,32 +2,38 @@ function make_pie(data) {
   var data_total = pv.sum(pv.map(data, function(x) { return x[1] }));
   data.sort(function(a,b) { return b[1] - a[1] });
   
-  var w = 180,
-      h = 180,
-      r = w / 2,
+  var c = 100,
+      r = c / 2,
       a = pv.Scale.linear(0, data_total).range(0, 2 * Math.PI);
   
   var vis = new pv.Panel()
-      .width(260)
-      .height(400);
+      .width(c)
+      .height(c);
 
   vis.add(pv.Wedge)
       .data(data)
       .top(r)
-      .left(r + 40)
+      .left(r)
       .outerRadius(r)
       .fillStyle(function(d) { return pv.Colors.category20().range()[this.index] })
       .angle(function(d) { return a(d[1]) })
       .title(function(d) { return d })
   
+  vis.render();
+}
+
+function pie_dot(data_point) {
+  var vis = new pv.Panel()
+      .width(14)
+      .height(14);
+  
   vis.add(pv.Dot)
-      .data(data)
-      .left(10)
-      .top(function() { return this.index * 12 + 220 })
+      .data([data_point])
+      .left(7)
+      .top(7)
       .fillStyle(function(d) { return pv.Colors.category20().range()[this.index] })
       .strokeStyle(null)
-    .anchor("right").add(pv.Label)
-      .text(function(d) { return d[0] });
 
   vis.render();
 }
+
