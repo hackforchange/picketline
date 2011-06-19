50d2940548871646fdfef473fd0b84e802ad5cf4
Andrew Farmer <andrew@moovweb.com>
2011-06-19 09:37:05 -0700

    added corpwatch api key

--- lib/api/corpwatch.rb
+++ lib/api/corpwatch.rb
@@ -2,11 +2,12 @@ require 'net/http'
 
 class CorpWatch
   HOST = 'api.corpwatch.org'
+  KEY = 'f9e4433920063c782d8f79ea6a55c890'
   
   class << self
     def children(id)
       res = Net::HTTP.start(HOST, 80) do |http|
-        http.get("/companies/#{id}/children.json")
+        http.get("/companies/#{id}/children.json?key=$#{KEY}")
       end
       results = []
       json = JSON.parse(res.body)
